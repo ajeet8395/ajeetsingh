1e8e13caf349df5a24e923dbe1420a3219e91e39
ajeelegodesk <ajeet@legodesk.com>
2025-06-11 12:21:54 +0530

    test

--- components/projects.tsx
+++ components/projects.tsx
@@ -24,7 +24,7 @@ const projects = [
   },
   {
     logo: "/englishexpresswayLogo.svg",
-    title: "iPhone",
+    title: "IPhone",
     description:
       "This is a clone of Apple's iPhone 15 Pro website using React.js and TailwindCSS. It highlights the effective use of GSAP (Greensock Animations) and Three.js for displaying iPhone 15 Pro models in various colors and shapes.",
     image: "/iphone.png",
